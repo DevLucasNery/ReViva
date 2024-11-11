0147d3eaefa677a70168edac3aeb9b388e0913e6
Lucas Franco <127855437+lucasfc-dev@users.noreply.github.com>
2024-11-11 20:59:07 -0300

    Versão que o mapa acha o local certo

--- reviva/app/components/mapa.tsx
+++ reviva/app/components/mapa.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import { useEffect, useState } from 'react'
@@ -13,15 +13,21 @@ L.Icon.Default.mergeOptions({
   shadowUrl,
 })
 
+
+const SetViewOnLocation = ({ position }: { position: [number, number] }) => {
+  const map = useMap()
+  useEffect(() => {
+    map.setView(position, 13) 
+  }, [map, position])
+  return null
+}
+
 interface MapaLeafletProps {
   initialPosition?: [number, number]
 }
 
-const MapaLeaflet: React.FC<MapaLeafletProps> = ({
-  initialPosition = [-23.55052, -46.633308],
-}) => {
+const MapaLeaflet: React.FC<MapaLeafletProps> = ({ initialPosition = [-23.55052, -46.633308] }) => {
   const [position, setPosition] = useState<[number, number] | null>(null)
-  const [map, setMap] = useState<L.Map | null>(null)
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -31,33 +37,25 @@ const MapaLeaflet: React.FC<MapaLeafletProps> = ({
           setPosition([latitude, longitude])
         },
         () => {
-          console.error('Não foi possível obter a localização.')
+          console.error("Não foi possível obter a localização.")
         }
       )
     }
   }, [])
 
-  useEffect(() => {
-    if (map && position) {
-      map.setView(position, 13)
-    }
-  }, [map, position])
-
   return (
-    <MapContainer
-      center={initialPosition}
-      zoom={13}
-      style={{ height: '75vh', width: '100%' }}
-      whenCreated={setMap}
-    >
+    <MapContainer center={initialPosition} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {position && (
-        <Marker position={position}>
-          <Popup>Você está aqui</Popup>
-        </Marker>
+        <>
+          <Marker position={position}>
+            <Popup>Você está aqui</Popup>
+          </Marker>
+          <SetViewOnLocation position={position} />
+        </>
       )}
     </MapContainer>
   )
